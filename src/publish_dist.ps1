8d75de2e7dd618608fd364ca8293ccbe453c4caa
Olivier MARTINET <om@activeassurances.fr>
2020-09-30 20:02:03 +0200

    udapte scripts

--- src/publish_dist.ps1
+++ src/publish_dist.ps1
@@ -1,3 +1,3 @@
-[string]$sourceDirectory  = "AllInBlog\dist\AllInBlog"
+[string]$sourceDirectory  = "AllInBlog\dist\AllInBlog\*"
 [string]$destinationDirectory = ".."
 Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
